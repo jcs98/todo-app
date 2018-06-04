ab907443503aa361c6cb1bd95d24d899977683fe
Jainam C Shah <jainamcshah98@gmail.com>
2018-06-04 17:49:18 +0530

    Add routing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Home from './components/Home';
 import { connect } from 'react-redux';
 import { addTodolist, deleteTodolist, toggleTodo, addTodo, deleteTodo, filterTodos } from './actions'
 
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
+
 import './App.css';
 
 
@@ -15,29 +17,63 @@ class App extends Component {
   render() {
 
     let todolists;
-    if(this.props.todolists) {
+    if (this.props.todolists) {
       todolists = this.props.todolists.map(todolist => {
         return (
-          <TodoList 
-          key={todolist.name} todolist={todolist} 
-          toggleTodo={this.props.toggleTodo} 
-          addTodo={this.props.addTodo} deleteTodo = {this.props.deleteTodo}
-          deleteTodolist={this.props.deleteTodolist}
-          currentFilter={this.props.currentFilter}/>
+          <TodoList
+            key={todolist.name} todolist={todolist}
+            toggleTodo={this.props.toggleTodo}
+            addTodo={this.props.addTodo} deleteTodo={this.props.deleteTodo}
+            deleteTodolist={this.props.deleteTodolist}
+            currentFilter={this.props.currentFilter} />
         );
       });
     }
 
     return (
+      <Router>
         <div className="App">
-          <h1>ToDo App</h1>
-          <AddList addTodolist={this.props.addTodolist}/>
-          <Home todolists={this.props.todolists}/>
-          <hr/>
-          <TodoFilter filterTodos={this.props.filterTodos}/>
-          <hr/>
-          {todolists}
+
+          <Route path="/" exact={true} render={() => {
+            return (
+              <div>
+                <h1>ToDo App</h1>
+                <AddList addTodolist={this.props.addTodolist} />
+                <Link to="/all-lists">All Lists</Link>
+                <Home todolists={this.props.todolists} />
+                <hr />
+              </div>);
+          }} />
+
+          <Route path="/all-lists" render={() => {
+            return (
+              <div>
+                <TodoFilter filterTodos={this.props.filterTodos} />
+                <hr />
+                {todolists}
+              </div>
+            );
+          }} />
+
+          <Route path="/todolist/:name" render={({ match }) => {
+            console.log(match.params.name);
+            let todolist = this.props.todolists.filter(todolist => todolist.name === match.params.name)[0];
+            return (
+              <div>
+                <TodoFilter filterTodos={this.props.filterTodos} />
+                <hr />
+                <TodoList
+                  key={todolist.name} todolist={todolist}
+                  toggleTodo={this.props.toggleTodo}
+                  addTodo={this.props.addTodo} deleteTodo={this.props.deleteTodo}
+                  deleteTodolist={this.props.deleteTodolist}
+                  currentFilter={this.props.currentFilter} />
+              </div>
+            );
+          }} />
+
         </div>
+      </Router>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
@@ -11,12 +12,14 @@ export default class Home extends Component {
           if(!todo.completed) pendingTodoCount++;
         });
         return (
+          <Link to={`/todolist/${todolist.name}`} key={todolist.name}>
           <li>
           <span style={{color:'black', fontSize:'32px'}}>{todolist.name + "      "} </span>
           <span style={{color:'blue',}}>All: {todolist.todos.length} </span>
           <span style={{color:'red',}}>Pending: {pendingTodoCount} </span>
           <span style={{color:'green',}}>Completed: {todolist.todos.length-pendingTodoCount}</span>
           </li>
+          </Link>
         );
       });
     }
